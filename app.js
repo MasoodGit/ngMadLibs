5d3151aea5adc63a87fde0754b658defb17f907f
MasoodGit <mohamed.alam78@gmail.com>
2015-07-22 17:03:57 +0530

    update add ngMessages, form submission, redirect views

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
-angular.module('madLibsApp',[])
+angular.module('madLibsApp',['ngMessages'])
 .controller('madLibsController',['$scope',function($scope) {
   $scope.words = {
-    male_name : "Male name",
-    dirty_task : "Dirty task",
-    obnoxious_celebrity : "Obnoxious celebrity",
-    job_title : "Job title",
-    celebrity : "Celebrity",
-    huge_number : "Huge Number",
-    tedious_task : "Tedious task",
-    useless_skill : "Useless skill",
-    adjective : "Adjective"
-  },
-  $scope.gender = "Male"
+    male_name : "",
+    dirty_task : "",
+    obnoxious_celebrity : "",
+    job_title : "",
+    celebrity : "",
+    huge_number : "",
+    tedious_task : "",
+    useless_skill : "",
+    adjective : ""
+  };
+  
+  $scope.gender = "Male";
+  
+  $scope.submitted = false;
+
+  $scope.interacted = function(field) {
+    return $scope.pickListForm.$submitted || field.$touched;
+  };
+
+  $scope.submit = function() {
+    if($scope.pickListForm.$valid) {
+      $scope.submitted = true;
+    }
+  };
+
+  $scope.reset = function() {
+    $scope.gender = "Female";
+    $scope.submitted = false;
+    for(var prop in $scope.words) {
+      $scope.words[prop] = "";
+    }
+    $scope.pickListForm.$setPristine();
+  };
 
 }]);
